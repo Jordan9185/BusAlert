d7fbf882b6fb5d6e21771c5cd8179494dea409d8
Chia cheng Lin <jordanlin@netappstore.net>
2018-02-06 16:49:32 +0800

    add BusEstimateTime and  BusEstimateTimeProvider model

---
+++ BusAlert/BusEstimateTime.swift
@@ -0,0 +1,16 @@
+//
+//  BusEstimateTime.swift
+//  BusAlert
+//
+//  Created by Chia cheng Lin on 2018/2/6.
+//  Copyright © 2018年 Chia cheng Lin. All rights reserved.
+//
+
+import Foundation
+
+struct BusEstimateTime {
+    let estimateTime: String
+    let goBack: String
+    let routeId: String
+    let stopId: String
+}
--- BusAlert/ViewController.swift
+++ BusAlert/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let qiaodongBusStopForBL15: String = "114984"
+    let qiaodongBusStopFor951: String = "180691"
+    var timer = DispatchSource.makeTimerSource(flags: [], queue: DispatchQueue.global())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.timer.schedule(deadline: DispatchTime.now(), repeating: .seconds(60), leeway: .milliseconds(10))
+
+        self.timer.setEventHandler(handler: {
+            DispatchQueue.main.sync {
+                self.getBusTime()
+            }
+        })
+        
+        self.timer.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func getBusTime() {
+        BusEstimateTimeProvider.share.delegate = self
+        BusEstimateTimeProvider.share.getBusBusEstimateTime(stopId: qiaodongBusStopForBL15)
+        BusEstimateTimeProvider.share.getBusBusEstimateTime(stopId: qiaodongBusStopFor951)
     }
-
-
 }
 
+extension ViewController: BusEstimateTimeProviderDelegate {
+    
+    func secondToMinute(second: Int)-> String {
+        if second == 0 {
+            return "已離站"
+        }
+        if second/60 == 0 {
+            return "再過 \(second) 秒 後抵達"
+        }
+        return "再過 \(second/60) 分 \(second%60) 秒 後抵達"
+    }
+    
+    func provider(prvider: BusEstimateTimeProvider, didGet busEstimateTimes: [BusEstimateTime]) {
+        
+        let busEstimateTime = busEstimateTimes.first!
+
+        if (busEstimateTime.estimateTime != "-1") {
+            
+            let estimateTime = secondToMinute(second: Int(busEstimateTime.estimateTime)!)
+            
+            if busEstimateTime.stopId == qiaodongBusStopFor951 {
+                print("951 \(estimateTime)")
+            } else if busEstimateTime.stopId == qiaodongBusStopForBL15  {
+                print("BL15 \(estimateTime)")
+            }
+        }
+    }
+    
+    func provider(prvider: BusEstimateTimeProvider, didFailWith error: BusEstimateTimeProviderError) {
+        print(error.localizedDescription)
+    }
+}
