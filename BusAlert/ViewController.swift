87431f55e8a4ad14aecfa9dbe057731326433629
Chia cheng Lin <jordanlin@netappstore.net>
2018-02-06 10:09:03 +0800

    Initial Commit

---
+++ BusAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusAlert
+//
+//  Created by Chia cheng Lin on 2018/2/6.
+//  Copyright © 2018年 Chia cheng Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
